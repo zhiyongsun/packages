ff620466157c3aa2d52cfe88c3f5eefa6dac56ee
sunzhiyong <dominick.szy@alibaba-inc.com>
2020-07-24 10:29:48 +0800

    feat: add first version

--- packages/iless/src/detect/ua.ts
+++ packages/iless/src/detect/ua.ts
@@ -1,11 +1,4 @@
-import detect from '@ali/anima-detect';
-
 export default  {
-  isAfwealth: function() {
-    let ua = window.navigator.userAgent
-    return /AFWealth/i.test(ua)
-  },
-
   isQQ: function() {
     let ua = window.navigator.userAgent.toLowerCase()
     return /qq/i.test(ua)
@@ -40,40 +33,4 @@ export default  {
     let ua = window.navigator.userAgent
     return /UCBrowser|UCBS|UCWEB/i.test(ua)
   },
-  /**
-   * 获取容器版本
-   * @return {string} 版本号
-   */
-  appVersion: function() {
-    let version = '';
-
-    if (detect.app && detect.app.version) {
-      version = detect.app.version;
-    }
-
-    return version;
-  },
-  /**
-   * [compareVersion description] 比较版本号大小
-   * @param  {[type]} versionA 目标版本A
-   * @param  {[type]} versionB    版本B
-   * @return {[type]} [description]
-   */
-  // versionB > versionA  => 1
-  // versionB < versionA => -1
-  // versionA === versionB => 0
-  compareVersion: function (versionA, versionB) {
-    const versionAarr = versionA.split('.');
-    const versionBarr = versionB.split('.');
-
-    for (let i = 0, n1, n2; i < versionBarr.length; i++) {
-      n1 = parseInt(versionAarr[i], 10) || 0;
-      n2 = parseInt(versionBarr[i], 10) || 0;
-
-      if (n1 > n2) return -1;
-      if (n1 < n2) return 1;
-    }
-
-    return 0;
-  }
 }
