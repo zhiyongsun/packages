f8b6e992d856d9df964cb2164037c31b0697439b
lanjing <dominick.szy@antgroup.com>
2022-01-04 16:28:06 +0800

    feat: 增加数组判断

--- packages/iless/src/array/array.ts
+++ packages/iless/src/array/array.ts
@@ -27,8 +27,67 @@ export function difference(a: any[], b: any[]) {
   return Array.from(new Set([...a].filter(x => !b.find(x))));
 }
 
+/**
+ * 判断是否是数组，通过 instanceof
+ * @param obj
+ * @returns {Boolean}
+ */
+export function isArrayByInstanceof(obj: any) {
+  return obj instanceof Array
+}
+
+/**
+ * 判断是否是数组，通过 constructor
+ * @param obj
+ * @returns {Boolean}
+ */
+export function isArrayByConstructor(obj: any) {
+  return obj && obj.constructor == Array
+}
+
+/**
+ * 判断是否是数组，通过 object.toString
+ * @param obj
+ * @returns {Boolean}
+ */
+export function isArrayByToString(obj: any) {
+  return Object.prototype.toString.call(obj).slice(8,-1) === 'Array';
+}
+
+/**
+ * 判断是否是数组，通过 Array.isArray
+ * @param obj
+ * @returns {Boolean}
+ */
+export function isArrayByIsArray(obj: any) {
+  return Array.isArray && Array.isArray(obj);
+}
+
+/**
+ * 判断是否是数组，通过 Array.prototype.isPrototypeOf
+ * @param obj
+ * @returns {Boolean}
+ */
+export function isArrayByIsPrototypeOf(obj: any) {
+  return Array.prototype.isPrototypeOf(obj);
+}
+
+/**
+ * 判断是否是数组，通过 __proto__
+ * @param obj
+ * @returns {Boolean}
+ */
+export function isArrayByProto(obj: any) {
+  return obj.__proto__ === Array.prototype;
+}
+
 export default {
   dedupe,
   union,
   difference,
+  isArrayByInstanceof,
+  isArrayByConstructor,
+  isArrayByToString,
+  isArrayByIsArray,
+  isArrayByProto,
 }
